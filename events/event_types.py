1dd86e357519a882a49f0f0764dbb407cdbfe8b4
Lukasz Prokulski <prokulski@gmail.com>
2022-04-23 13:43:04 +0200

    usunięcie zbędnych plików

--- events/event_types.py
+++
@@ -1,3 +0,0 @@
-from events.events_routings import customer_buys_product, new_customer, new_product
-
-event_types = {"new_customer": new_customer, "new_product": new_product, "customer_buys_product": customer_buys_product}
